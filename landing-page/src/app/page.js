6abe457ed4c1af945a7879ddd78e213e8125fd05
Vigoraj_bty <bvigoraj@gmail.com>
2025-10-31 19:35:52 +0530

    Update page.js

--- landing-page/src/app/page.js
+++ landing-page/src/app/page.js
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
